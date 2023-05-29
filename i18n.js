bcddb829a6266f222520db6817e89a4ea769b343
ValentineSokol <vallyull98@gmail.com>
2023-05-29 16:20:01 +0300

    Configure Translation loading for new routes

--- i18n.js
+++ i18n.js
@@ -2,6 +2,10 @@ module.exports = {
     locales: ['en', 'uk'],
     defaultLocale: 'en',
     pages: {
+        '/': ['landing', 'register'],
         '*': ['footer'],
+        '/landing': ['landing', 'register'],
+        '/404': ['404'],
+        '/register': ['register']
     },
 }
