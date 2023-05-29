9b2981e9c963a0d2d2ec0e26a1faa225ea3176ad
ValentineSokol <vallyull98@gmail.com>
2023-05-29 16:19:16 +0300

    Added the landing card constants

---
+++ consts/landing/landingCards.js
@@ -0,0 +1,22 @@
+import { faBook } from '@fortawesome/free-solid-svg-icons/faBook';
+import { faComments } from '@fortawesome/free-solid-svg-icons/faComments';
+import { faHeadphones } from '@fortawesome/free-solid-svg-icons/faHeadphones';
+
+
+export const landingCards = [
+    {
+        icon: faBook,
+        heading: 'cards.offline.heading',
+        text: 'cards.offline.text'
+    },
+    {
+        icon: faComments,
+        heading: 'cards.chat.heading',
+        text: 'cards.chat.text'
+    },
+    {
+        icon: faHeadphones,
+        heading: 'cards.tts.heading',
+        text: 'cards.tts.text'
+    },
+];
