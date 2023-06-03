8c8785e06791573bb351e06becf8685f2e88354f
ValentineSokol <vallyull98@gmail.com>
2023-06-03 08:52:32 +0300

    Decouple Input from react-hook-form

---
+++ components/Input/FormInput.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Input, InputProps } from "@/components/Input/Input";
+import {UseFormReturn} from "react-hook-form";
+
+export interface FormInputProps extends InputProps {
+    formAPI: UseFormReturn,
+    name: string,
+}
+export const FormInput = ({ name, formAPI, ...props } : FormInputProps) => {
+    return <Input  {...props} {...formAPI.register(name)} focusCb={() => formAPI.setFocus(name) } />
+}
--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,51 +1,54 @@
-import React, { HTMLAttributes, ReactElement, useId} from "react";
+import React, {HTMLAttributes, forwardRef, ReactElement, useId, ForwardedRef} from "react";
 import styles from './Input.module.scss';
 import clsx from "clsx";
 import {ErrorText} from "../ErrorText/ErrorText";
-import { UseFormReturn} from "react-hook-form";
-import {useAutoFocus} from "@/utils/hooks/useAutoFocus";
+import {AutoFocusProps, useAutoFocus} from "@/utils/hooks/useAutoFocus";
 
 import { Text } from "@/components/Text/Text";
 
-export interface InputProps extends HTMLAttributes<HTMLInputElement> {
-    autoFocus?: boolean,
+export interface InputProps extends HTMLAttributes<HTMLInputElement>, AutoFocusProps {
     label: string,
+    slotAfterLabel?: ReactElement,
+    labelVisuallyHidden?: boolean,
     name: string,
     error?: string,
     fullWidth?: boolean,
     wrapClassName?: string,
     variant?: 'normal' | 'filled',
     renderLabel?: (label : ReactElement) => ReactElement,
-    formAPI : UseFormReturn
-
 }
-export const Input = ({ variant = 'normal', autoFocus, wrapClassName, fullWidth,  label, formAPI, name, renderLabel, error, ...props } : InputProps) => {
-    const id = useId();
+export const Input = forwardRef(({ id, variant = 'normal', onChange, autoFocus, focusCb, wrapClassName, fullWidth,  label, renderLabel, slotAfterLabel = null, labelVisuallyHidden = false, error, ...props } : InputProps, ref: ForwardedRef<HTMLInputElement>) => {
+    const defaultId = useId();
     const errorId = useId();
-    useAutoFocus<HTMLInputElement>(autoFocus, { focusCb: () => formAPI.setFocus(name) });
+    useAutoFocus<HTMLInputElement>(autoFocus, { focusCb, customRef: ref });
 
-    const labelNode = <Text Tag='label' htmlFor={id}>{`${label}:`}</Text>;
+    const labelNode = <Text Tag='label' htmlFor={id || defaultId}>{`${label}:`}</Text>;
+    const labelContent = renderLabel ? renderLabel(labelNode) : labelNode;
     return (
         <div className={wrapClassName}>
             {
-                renderLabel ?
-                <div className={styles[`label_${variant}`]} >{renderLabel(labelNode)}</div>
-                    :
-                <div className={styles[`label_${variant}`]}>{labelNode}</div>
+                !labelVisuallyHidden &&
+                <div className={clsx(styles.label, styles[`label_${variant}`])}>{labelContent}</div>
             }
+            {slotAfterLabel}
             <input
-                aria-describedby={error ? errorId : ''}
-                {...formAPI.register(name)}
+                aria-label={labelVisuallyHidden && label}
+                ref={ref}
+                {...(error ? { ['aria-describedby']: errorId } : {})}
                 aria-invalid={!!error}
-                id={id}
+                id={id || defaultId}
+                onChange={onChange}
                 className={clsx(
                     styles[variant],
                     error && styles.input_error,
                     fullWidth && styles.fullWidth,
                     'fs-3'
-                )} {...props} />
+                )}
+                {...props}
+            />
             { error && <ErrorText id={errorId} className={styles.error}>{error}</ErrorText> }
         </div>
     )
+});
 
-};
+Input.displayName = "Input";
