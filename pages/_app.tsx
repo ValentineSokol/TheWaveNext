4ae5ef72026d9c8e07d7a82cf15af7cba7f4ee9c
ValentineSokol <vallyull98@gmail.com>
2023-06-03 08:43:25 +0300

    Persist user-selected language in a cookie

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,36 @@ import '@/styles/globals.css';
 import '@/styles/font_scale.scss';
 
 import Footer from '../components/Footer/Footer';
+import {TopNavigation} from "@/components/TopNavigation/TopNavigation";
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from "next/router";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {RegisterModal} from "@/components/RegisterForm/RegisterForm";
+import {useEffect} from "react";
 
 export default function App({Component, pageProps}) {
+
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const { locale } = router;
+
+    useEffect(() => {
+        saveUserLanguage();
+    }, [locale]);
+
+    const saveUserLanguage = () => {
+        const currentLocale = document.cookie.split('=')[1];
+        if (currentLocale === locale) return;
+        document.cookie = `NEXT_LOCALE=${locale};path=/`;
+    }
+    const closeTopLevelModals = () => {
+        router.replace(window.location.pathname, undefined);
+    }
     return (
         <QueryClientProvider client={new QueryClient()}>
+            <RegisterModal onClose={closeTopLevelModals} open={searchParams.get('showAuthModal') === 'true'} />
+            <TopNavigation />
+            <ReactQueryDevtools />
             <Component {...pageProps} />
             <Footer />
         </QueryClientProvider>
