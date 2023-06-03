684a778178a0254c406f53c4a78239e676237000
ValentineSokol <vallyull98@gmail.com>
2023-06-03 08:58:25 +0300

    refactor Button to support new Text and Spinner components

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,13 +1,14 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import React, {ButtonHTMLAttributes, forwardRef} from 'react';
 import styles from "./Button.module.scss";
 import clsx from "clsx";
 import {Spinner} from "../Spinner/Spinner";
 import {useAutoFocus} from "@/utils/hooks/useAutoFocus";
-import  { Text } from "@/components/Text/Text";
-import { LeadingIcon, WithIconProp } from "@/components/LeadingIcon/LeadingIcon";
+import {FontSizeProp, Text} from "@/components/Text/Text";
+import { WithIconProp} from "@/components/LeadingIcon/LeadingIcon";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, WithIconProp {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, WithIconProp, FontSizeProp {
     autoFocus?: boolean,
+    textClassName?: string,
     size?: 's' | 'm' | 'l',
     variant?: 'primary' | 'accent' | 'transparent' | 'link_light' | 'link_dark' | 'link_accent'
     isLoading?: boolean,
@@ -20,35 +21,50 @@ const fontSizes = {
     m: 4,
     l: 5
 };
-export const Button = ({ autoFocus, size = 'm', icon, iconPosition, isLoading, spinnerHideDelay, children, className, variant = 'primary', ...props} : ButtonProps) => {
+export const Button = forwardRef(({
+                           fontSize,
+                           textClassName,
+                           autoFocus,
+                           size = 'm',
+                           icon,
+                           iconPosition,
+                           isLoading,
+                           spinnerHideDelay,
+                           children,
+                           className,
+                           variant = 'primary',
+                           ...props
+                       }: ButtonProps, ref) => {
 
-    const autoFocusRef = useAutoFocus<HTMLButtonElement>(autoFocus);
+    const autoFocusRef = useAutoFocus<HTMLButtonElement>(autoFocus, { customRef: ref });
     const disabled = props.disabled || isLoading;
 
     return (
         <button
-        ref={autoFocusRef}
-        disabled={disabled}
-        className={clsx(
-            className,
-            styles.common,
-            styles[variant],
-            styles[`size_${size}`],
-            disabled && styles.disabled,
-        )}
-        {...props}
-    >
-        <Spinner isLoading={isLoading}>
+            ref={ref || autoFocusRef}
+            disabled={disabled}
+            className={clsx(
+                className,
+                styles.common,
+                styles[variant],
+                styles[`size_${size}`],
+                disabled && styles.disabled,
+            )}
+            {...props}
+        >
             {
-                <Text
-                    fontSize={fontSizes[size]}
-                    Tag='div'
-                >
-                <LeadingIcon icon={icon} iconPosition={iconPosition}>
-                    {children}
-                </LeadingIcon>
-            </Text>}
-        </Spinner>
-    </button>
+                isLoading ?
+                    <Spinner/> :
+                    <Text
+                        childClassName={textClassName}
+                        icon={icon}
+                        iconPosition={iconPosition}
+                        fontSize={fontSize || fontSizes[size]}
+                        Tag='div'
+                    >
+                        {children}
+                    </Text>
+            }
+        </button>
     );
-}
+});
