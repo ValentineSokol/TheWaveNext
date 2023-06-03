9e51bca662c6fc85b43e5a8ef01764a5c91f2da0
ValentineSokol <vallyull98@gmail.com>
2023-06-03 08:45:39 +0300

    useAutoFocus: support custom refs

--- utils/hooks/useAutoFocus.ts
+++ utils/hooks/useAutoFocus.ts
@@ -1,13 +1,17 @@
 import {Ref, useEffect, useRef} from "react";
 
 
-export function useAutoFocus<T extends  HTMLElement>(autoFocus: boolean, { focusCb }: { focusCb?: () => void } = {}) : Ref<T> {
+export interface AutoFocusProps {
+    autoFocus?: boolean,
+    focusCb?: () => void,
+}
+export function useAutoFocus<T extends  HTMLElement>(autoFocus: boolean, { focusCb, customRef }: { focusCb?: () => void, customRef?: any } = {}) : Ref<T> {
     const ref = useRef<T>(null);
     const focusTimeout = useRef(null);
-    
+
     useEffect(() => {
         if (!autoFocus) return;
-        const defaultCb = () => ref.current.focus();
+        const defaultCb = () => (customRef || ref).current.focus();
         // in some cases we need to defer focusing until after the element becomes visible <vSokol>
         focusTimeout.current = setTimeout(focusCb || defaultCb, 0);
         return () => clearTimeout(focusTimeout.current);
