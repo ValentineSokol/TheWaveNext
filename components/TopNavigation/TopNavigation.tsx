6b1656954f1364525d45b43154468db3207fd852
ValentineSokol <vallyull98@gmail.com>
2023-06-03 19:00:48 +0300

    Improve top navigation responsiveness and a11y

---
+++ components/TopNavigation/TopNavigation.tsx
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import Link from 'next/link';
+import {useCurrentUser} from "@/api/UserApi";
+import {Button} from "@/components/Button/Button";
+import {NAV_LINKS} from "@/components/TopNavigation/navLinks";
+import styles from './TopNavigation.module.scss';
+import {Text} from "@/components/Text/Text";
+import {faRightFromBracket} from "@fortawesome/free-solid-svg-icons/faRightFromBracket";
+import {SearchInput} from "@/components/SearchInput/SearchInput";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons/faMagnifyingGlass";
+import {faArrowLeftLong} from "@fortawesome/free-solid-svg-icons/faArrowLeftLong";
+import Image from "next/image";
+import logo from '@/assets/logo.png';
+
+
+export const TopNavigation = () => {
+    const {user} = useCurrentUser();
+    const [showMobileSearch, setShowMobileSearch] = useState(false);
+
+    const onSearchButtonClick = () => setShowMobileSearch(prev => !prev);
+
+    return (
+        <nav className={styles.nav}>
+            <div className={styles.topBar}>
+                { !showMobileSearch &&
+                <div className={styles.logoWithText}>
+                    <Image className={styles.logo} src={logo} alt='Logo'/>
+                    <Text fontSize={4}>The Fanfic Wave</Text>
+                </div>
+                }
+                <Button className={styles.mobileSearchToggle}
+                        icon={showMobileSearch ? faArrowLeftLong : faMagnifyingGlass} onClick={onSearchButtonClick}
+                        aria-label='Search Wave'/>
+                {
+                    showMobileSearch &&
+                    <SearchInput fullWidth multiSelect={false} labelVisuallyHidden
+                                 placeholder='type to start a new search'/>
+                }
+            </div>
+            {
+                !showMobileSearch &&
+                <ul className={styles.list}>
+                        {NAV_LINKS(user).map((link) => (
+                            <li key={link.href}>
+                                <Link className={styles.link} href={link.href}><Text childClassName={styles.linkText}
+                                                                                     fontSize={4}
+                                                                                     icon={link.icon}>{link.text}</Text></Link>
+                            </li>
+                        ))}
+                    {user?.isLoggedIn &&
+                        <li className={styles.logOutBtn}><Button textClassName={styles.linkText} icon={faRightFromBracket} fontSize={4} size='s'
+                                    variant="link_light">Log Out</Button></li>
+                    }
+                </ul>
+            }
+        </nav>
+    )
+}
