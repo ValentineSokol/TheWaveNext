255dd65b12ee66d9a251740c131972b15c263627
ValentineSokol <vallyull98@gmail.com>
2023-06-03 08:50:29 +0300

    Support making an icon button in Text

--- components/Text/Text.tsx
+++ components/Text/Text.tsx
@@ -1,14 +1,22 @@
-import React, {JSX, HTMLProps, ReactNode} from "react";
+import React, {JSX, HTMLProps, ReactNode, ReactElement} from "react";
 import clsx from "clsx";
+import { LeadingIcon, WithIconProp } from "@/components/LeadingIcon/LeadingIcon";
 
 export interface FontSizeProp {
     fontSize?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8,
 }
-interface TextProps extends HTMLProps<any>, FontSizeProp {
-    children?: ReactNode,
+export interface TextProps extends HTMLProps<any>, FontSizeProp, WithIconProp {
+    children?: ReactElement,
     className?: string,
+    childClassName?: string,
     Tag?: keyof JSX.IntrinsicElements
 }
-export const Text = ({ children, className, Tag = 'span', fontSize = 3, ...props }: TextProps) => {
-    return <Tag className={clsx(className,`fs-${fontSize}`)} {...props}>{children}</Tag>
+export const Text = ({ children, className, childClassName, icon, onIconClick, iconPosition, iconAriaLabel, Tag = 'span', fontSize = 3, ...props }: TextProps) => {
+    return (
+        <Tag className={clsx(className,`fs-${fontSize}`)} {...props}>
+            <LeadingIcon iconAriaLabel={iconAriaLabel} icon={icon} iconPosition={iconPosition} onIconClick={onIconClick}>
+                <span className={childClassName}>{children}</span>
+            </LeadingIcon>
+        </Tag>
+    );
 }
