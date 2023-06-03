df5197c7fe05f520c1c7dd3a916c6fae8ebbccc3
ValentineSokol <vallyull98@gmail.com>
2023-06-03 08:51:37 +0300

    Support making  icon buttons in LeadingIcon.tsx

--- components/LeadingIcon/LeadingIcon.tsx
+++ components/LeadingIcon/LeadingIcon.tsx
@@ -3,23 +3,27 @@ import React, { ReactElement } from "react";
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
 
 import styles from './LeadingIcon.module.scss';
+import {Button} from "@/components/Button/Button";
 
 export interface WithIconProp {
     icon?: IconProp,
+    onIconClick?: () => void,
+
+    iconAriaLabel?:string,
     iconPosition?: 'leading' | 'trailing'
 }
 interface LeadingIconProps extends WithIconProp{
     children: ReactElement,
 }
-export const LeadingIcon = ({ children, icon, iconPosition = 'leading' } : LeadingIconProps) : ReactElement => {
+export const LeadingIcon = ({ children, icon, onIconClick, iconAriaLabel, iconPosition = 'leading' } : LeadingIconProps) : ReactElement => {
     if (!icon) return children;
 
-    const renderedIcon = <FontAwesomeIcon className={styles[iconPosition]} icon={icon}/>;
+    const renderedIcon = onIconClick ? <Button variant='transparent' onClick={onIconClick} aria-label={iconAriaLabel} size='s' icon={icon} /> :  <FontAwesomeIcon className={styles[iconPosition]} icon={icon}/>;
     const content = [renderedIcon, children];
 
     return (
         <>
-            {iconPosition === 'leading' ? content : [...content].reverse()}
+            <div style={{ display: 'flex', alignItems: 'center' }}>{iconPosition === 'leading' ? content : [...content].reverse()}</div>
         </>
     );
 }
