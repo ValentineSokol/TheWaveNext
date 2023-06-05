fbf2159b63a93a14863699fa403bcde4f78010d7
ValentineSokol <vallyull98@gmail.com>
2023-06-05 18:36:43 +0300

    Add a service worker

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-const nextTranslate = require('next-translate-plugin')
+const nextTranslate = require('next-translate-plugin');
+const withPWA = require("@imbios/next-pwa")({
+    dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: false,
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true, // workaround, I need to fix those
+    },
+    distDir: 'build',
 }
 
-module.exports = nextTranslate(nextConfig);
+module.exports = nextTranslate(withPWA(nextConfig));
