bed8c1789e42a4427fc49798bd10676c63576f99
ValentineSokol <vallyull98@gmail.com>
2023-06-07 13:22:02 +0300

    Add Story Api to handle stories.

--- api/StoryApi.js
+++ api/StoryApi.js
@@ -1,6 +1,15 @@
 import request from "@/api/request";
-import {useQuery} from "@tanstack/react-query";
+import {useMutation, useQuery} from "@tanstack/react-query";
 
 export const StoryApi = {
-    search: (query, entity) => request.post('/stories/search', { entity, query })
+    search: (query, entity) => request.post('/stories/search', { entity, query }),
+    post: (payload) => request.post('/stories', payload)
 };
+
+export const usePostStory = () => {
+    const mutation = useMutation({
+        mutationFn: (payload) => StoryApi.post(payload)
+    });
+
+    return mutation;
+}
