e908a7bf5b00566fa6d3f03a95406a7494d90dd0
ValentineSokol <vallyull98@gmail.com>
2023-06-15 14:09:56 +0300

    fix lint in Editor

--- components/Editor/Editor.tsx
+++ components/Editor/Editor.tsx
@@ -1,26 +1,31 @@
-import React, {useId} from 'react';
+import React, { useId } from 'react';
 import dynamic from 'next/dynamic';
+import { ErrorText } from '@/components/ErrorText/ErrorText';
+
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import styles from './Editor.module.scss';
 
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import {ErrorText} from "@/components/ErrorText/ErrorText";
-
 const ReactDraftEditor = dynamic(
-    () => import('react-draft-wysiwyg').then((mod) => mod.Editor),
-    { ssr: false }
+  () => import('react-draft-wysiwyg').then((mod) => mod.Editor),
+  { ssr: false },
 );
-export const Editor = ({ error, ...props }) => {
-    const errorId = useId();
-    const toolbar = {
-        options:  ['inline', 'textAlign', 'history'],
-        inline: {
-            options: ['bold', 'italic', 'underline', 'strikethrough']
-        }
-    };
-    return (
-        <>
-        <ReactDraftEditor editorClassName={styles.editor} toolbar={toolbar} toolbarOnFocus {...props} />
-        {error && <ErrorText id={errorId}>{error}</ErrorText>}
-        </>
-    );
+export function Editor({ error, ...props }) {
+  const errorId = useId();
+  const toolbar = {
+    options: ['inline', 'textAlign', 'history'],
+    inline: {
+      options: ['bold', 'italic', 'underline', 'strikethrough'],
+    },
+  };
+  return (
+    <>
+      <ReactDraftEditor
+        editorClassName={styles.editor}
+        toolbar={toolbar}
+        toolbarOnFocus
+        {...props}
+      />
+      {error && <ErrorText id={errorId}>{error}</ErrorText>}
+    </>
+  );
 }
--- components/Editor/FormEditor.tsx
+++ components/Editor/FormEditor.tsx
@@ -1,15 +1,31 @@
-import React from "react";
-import {Editor} from "@/components/Editor/Editor";
-import {Controller} from "react-hook-form";
+import React from 'react';
+import { Editor } from '@/components/Editor/Editor';
+import { Controller } from 'react-hook-form';
 import { convertToRaw } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
 
-export const FormEditor = ({ name, control, ...props }) => {
+export function FormEditor({
+  name,
+  control,
+  error,
+  ...props
+}) {
+  const toHTML = (state) => () => draftToHtml(convertToRaw(state.getCurrentContent()));
+  const toPlainText = (state) => () => state.getCurrentContent().getPlainText();
+  const onEditorStateChange = (field) => (state) => field.onChange({
+    state,
+    toHTML: toHTML(state),
+    toPlainText: toPlainText(state),
+  });
+  const renderEditor = ({ field }) => (
+    <Editor
+      error={error}
+      onBlur={field.onBlur}
+      editorState={field.value?.state}
+      onEditorStateChange={onEditorStateChange(field)}
+      {...props}
+    />
+  );
 
-    const toHTML = (state) => () => draftToHtml(convertToRaw(state.getCurrentContent()));
-    const toPlainText = (state) => () => state.getCurrentContent().getPlainText();
-    const onEditorStateChange = (field) => (state) => field.onChange({ state, toHTML: toHTML(state), toPlainText: toPlainText(state) });
-    const renderEditor = ({ field }) => <Editor onBlur={field.onBlur} editorState={field.value?.state} onEditorStateChange={onEditorStateChange(field)} {...props} />
-
-    return <Controller control={control} render={renderEditor} name={name} />
+  return <Controller control={control} render={renderEditor} name={name} />;
 }
