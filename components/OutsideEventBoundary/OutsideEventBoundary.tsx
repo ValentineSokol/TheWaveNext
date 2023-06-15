1a54cec867e22f568c89bb902febabad77ddb504
ValentineSokol <vallyull98@gmail.com>
2023-06-15 14:26:19 +0300

    fix lint in OutsideEventBoundary.tsx

--- components/OutsideEventBoundary/OutsideEventBoundary.tsx
+++ components/OutsideEventBoundary/OutsideEventBoundary.tsx
@@ -1,36 +1,36 @@
-import React, {useEffect, useRef } from "react";
+/* eslint-disable jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */
+import React, { useEffect, useRef } from 'react';
 
 const trackedEvents = ['click', 'focusin'];
-export const OutsideEventBoundary = ({ onOutsideEvent, children }) => {
-    const isInsideEventRef = useRef(false);
+export function OutsideEventBoundary({ onOutsideEvent, children }) {
+  const isInsideEventRef = useRef(false);
 
-    const handleEventGlobal = (e) => {
-        if (isInsideEventRef.current) {
-            isInsideEventRef.current = false;
-            return;
-        }
-        onOutsideEvent();
+  const handleEventGlobal = () => {
+    if (isInsideEventRef.current) {
+      isInsideEventRef.current = false;
+      return;
     }
-    const toggleGlobalListeners = (attach) => {
-        const action = attach ? 'add' : 'remove';
-        const method = `${action}EventListener`;
+    onOutsideEvent();
+  };
+  const toggleGlobalListeners = (attach) => {
+    const action = attach ? 'add' : 'remove';
+    const method = `${action}EventListener`;
 
-        trackedEvents.forEach(event => {
-            window.document[method](event, handleEventGlobal);
-        });
-    }
-    const handleInsideEvent = (e) => {
-        isInsideEventRef.current = true;
-    }
+    trackedEvents.forEach((event) => {
+      window.document[method](event, handleEventGlobal);
+    });
+  };
+  const handleInsideEvent = () => {
+    isInsideEventRef.current = true;
+  };
 
-    useEffect(() => {
-        toggleGlobalListeners(true);
-
-        return toggleGlobalListeners;
-    }, [isInsideEventRef.current]);
-    return (
+  useEffect(() => {
+    toggleGlobalListeners(true);
+    return () => toggleGlobalListeners(false);
+  }, [isInsideEventRef.current]);
+  return (
     <div onClick={handleInsideEvent} onFocus={handleInsideEvent}>
-        {children}
+      {children}
     </div>
-    );
+  );
 }
