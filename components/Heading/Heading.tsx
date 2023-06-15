567318fae2f49c829f425051ab368b1fb84d90dd
ValentineSokol <vallyull98@gmail.com>
2023-06-15 14:15:03 +0300

    fix lint in Heading

--- components/Heading/Heading.tsx
+++ components/Heading/Heading.tsx
@@ -1,33 +1,34 @@
-import React, {HTMLAttributes, ReactNode} from "react";
+import React, { HTMLAttributes } from 'react';
+
+import { FontSize, Text } from '@/components/Text/Text';
+
+import { LeadingIcon, WithIconProp } from '@/components/LeadingIcon/LeadingIcon';
+import clsx from 'clsx';
 import styles from './Heading.module.scss';
 
-import {Text} from "@/components/Text/Text";
-
-import { LeadingIcon, WithIconProp } from "@/components/LeadingIcon/LeadingIcon";
-import clsx from "clsx";
-
 interface HeadingProps extends HTMLAttributes<HTMLHeadingElement>, WithIconProp {
-    level?: 1 | 2 | 3 | 4 | 5 | 6,
-    fontSize?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+  level?: 1 | 2 | 3 | 4 | 5 | 6,
+  fontSize?: FontSize
 }
 
 const defaultFontSize = {
-    1: 8,
+  1: 8,
 };
-export const Heading = ({children, className, icon, iconPosition, level = 1, fontSize = 4, ...props}: HeadingProps) => {
-    const Tag: keyof JSX.IntrinsicElements = `h${level}`;
+export function Heading({
+  children, className, icon, iconPosition, level = 1, fontSize = 4, ...props
+}: HeadingProps) {
+  const Tag: keyof JSX.IntrinsicElements = `h${level}`;
 
-
-    return (
-        <Text
-            className={clsx(styles.heading, className)}
-            fontSize={fontSize || defaultFontSize[level]}
-            Tag={Tag}
-            {...props}
-        >
-            <LeadingIcon icon={icon} iconPosition={iconPosition}>
-            {children}
-            </LeadingIcon>
-        </Text>
-    );
+  return (
+    <Text
+      className={clsx(styles.heading, className)}
+      fontSize={fontSize || defaultFontSize[level]}
+      Tag={Tag}
+      {...props}
+    >
+      <LeadingIcon icon={icon} iconPosition={iconPosition}>
+        {children}
+      </LeadingIcon>
+    </Text>
+  );
 }
