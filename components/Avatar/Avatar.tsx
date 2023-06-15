6c41e0f624a42ee070936665003c9ea193557bfe
ValentineSokol <vallyull98@gmail.com>
2023-06-15 13:58:11 +0300

    fix lint in Avatar

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import Image from "next/image";
+import Image from 'next/image';
 import defaultAvatar from '@/assets/defaultAvatar.jpg';
-import {AVATAR_HEIGHT, AVATAR_WIDTH} from "@/consts/profile/avatar";
+import { AVATAR_HEIGHT, AVATAR_WIDTH } from '@/consts/profile/avatar';
 
-export const Avatar = ({ url }) => {
-    const [src, setSrc] = useState(url || defaultAvatar);
+export function Avatar({ url }) {
+  const [src, setSrc] = useState(url || defaultAvatar);
 
-    const onError = () => {
-        if (src !== url) return;
-        setSrc(defaultAvatar);
-    }
-    return <Image width={AVATAR_WIDTH} height={AVATAR_HEIGHT} src={src} onError={onError} alt="" />
+  const onError = () => {
+    if (src !== url) return;
+    setSrc(defaultAvatar);
+  };
+  return <Image width={AVATAR_WIDTH} height={AVATAR_HEIGHT} src={src} onError={onError} alt="" />;
 }
