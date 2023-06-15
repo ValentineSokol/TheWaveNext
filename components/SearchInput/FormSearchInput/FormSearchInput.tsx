e7f035b3166c8c0be5d1108157598ddf11681ef7
ValentineSokol <vallyull98@gmail.com>
2023-06-15 14:59:31 +0300

    fixed lint for SearchInput.tsx

--- components/SearchInput/FormSearchInput/FormSearchInput.tsx
+++ components/SearchInput/FormSearchInput/FormSearchInput.tsx
@@ -1,20 +1,30 @@
-import React from "react";
-import {SearchInput, SearchInputProps} from "@/components/SearchInput/SearchInput";
-import {Controller, ControllerProps} from "react-hook-form";
+import React from 'react';
+import { SearchInput, SearchInputProps } from '@/components/SearchInput/SearchInput';
+import { Controller, ControllerProps } from 'react-hook-form';
 
 interface FormSearchInputProps extends SearchInputProps, ControllerProps {
-    onResultItemSelect?: () => void,
-    onResultItemRemove?: () => void,
+  onResultItemSelect?: () => void,
+  onResultItemRemove?: () => void,
 }
-export const FormSearchInput = ({ control,  error, onResultItemSelect, onResultItemRemove, name, ...props }: FormSearchInputProps) => {
-
-    const onChange = (field, add = true) => (selectedItems, id) => {
-        const cb = add ? onResultItemSelect : onResultItemRemove;
-        field.onChange(selectedItems);
-        cb?.(selectedItems, id);
-    }
-    const renderInput = ({ field }) => <SearchInput error={error} onBlur={field.onBlur} selectedValues={field.value} onResultSelect={onChange(field)} onResultRemove={onChange(field, false)} {...props} />;
-    return (
-       <Controller defaultValue={[]} name={name} control={control} render={renderInput} />
-    )
+export function FormSearchInput({
+  control, error, onResultItemSelect, onResultItemRemove, name, ...props
+}: FormSearchInputProps) {
+  const onChange = (field, add = true) => (selectedItems, id) => {
+    const cb = add ? onResultItemSelect : onResultItemRemove;
+    field.onChange(selectedItems);
+    cb?.(selectedItems, id);
+  };
+  const renderInput = ({ field }) => (
+    <SearchInput
+      error={error}
+      onBlur={field.onBlur}
+      selectedValues={field.value}
+      onResultSelect={onChange(field)}
+      onResultRemove={onChange(field, false)}
+      {...props}
+    />
+  );
+  return (
+    <Controller defaultValue={[]} name={name} control={control} render={renderInput} />
+  );
 }
