1e7ee7a380e809ac82263f44dd3537b4bcc584cb
ValentineSokol <vallyull98@gmail.com>
2023-06-15 14:34:35 +0300

    added storybook for PasswordInput.tsx

---
+++ components/PasswordInput/PasswordInput.stories.tsx
@@ -0,0 +1,12 @@
+import { PasswordInput } from './PasswordInput';
+import * as InputStories from '../Input/Input.stories';
+
+const meta = {
+  title: 'PasswordInput',
+  component: PasswordInput,
+};
+export default meta;
+
+export const Default = {
+  args: InputStories.Default.args,
+};
