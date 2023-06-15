b9442aebe3442a77b853d2ef1d77d305d2864465
ValentineSokol <vallyull98@gmail.com>
2023-06-15 14:58:57 +0300

    fixed lint for pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import React from "react";
+import {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+import React from 'react';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-          <link rel="manifest" href='/manifest.json' />
-          <meta name='theme-color' content="#1c70c0" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#1c70c0" />
       </Head>
       <body>
         <div id="modalRoot" />
@@ -14,5 +16,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
