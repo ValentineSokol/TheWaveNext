1353f6ff3096ae7aa8bceb8df1186eeffecedf31
ValentineSokol <vallyull98@gmail.com>
2023-06-15 15:00:21 +0300

    fixed lint for Spinner.tsx

--- components/Spinner/Spinner.tsx
+++ components/Spinner/Spinner.tsx
@@ -1,6 +1,10 @@
-import React, {HTMLAttributes} from 'react';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faSpinner} from "@fortawesome/free-solid-svg-icons/faSpinner";
-import {Text} from "@/components/Text/Text";
+import React, { HTMLAttributes } from 'react';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons/faSpinner';
+// TODO: fix this cycle
+// eslint-disable-next-line import/no-cycle
+import { Text } from '@/components/Text/Text';
 import styles from './Spinner.module.scss';
-export const Spinner = ({ ...props } : HTMLAttributes<HTMLSpanElement>) => <Text className={styles.spinner} {...props} ><FontAwesomeIcon icon={faSpinner} spin /></Text>;
+
+export function Spinner({ ...props } : HTMLAttributes<HTMLSpanElement>) {
+  return <Text icon={faSpinner} iconProps={{ spin: true }} className={styles.spinner} {...props} />;
+}
