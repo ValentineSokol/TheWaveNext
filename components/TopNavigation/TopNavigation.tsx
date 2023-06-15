9b48e625492c0edb04f88544df6bac0d09558456
ValentineSokol <vallyull98@gmail.com>
2023-06-15 14:51:11 +0300

    fixed lint for TopNavigation.tsx

--- components/TopNavigation/TopNavigation.tsx
+++ components/TopNavigation/TopNavigation.tsx
@@ -1,75 +1,98 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
-import useTranslation from "next-translate/useTranslation";
-import {useCurrentUser} from "@/api/UserApi";
-import {Button} from "@/components/Button/Button";
-import {NAV_LINKS} from "@/components/TopNavigation/navLinks";
-import styles from './TopNavigation.module.scss';
-import {Text} from "@/components/Text/Text";
-import {faRightFromBracket} from "@fortawesome/free-solid-svg-icons/faRightFromBracket";
-import {SearchInput} from "@/components/SearchInput/SearchInput";
-import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons/faMagnifyingGlass";
-import {faArrowLeftLong} from "@fortawesome/free-solid-svg-icons/faArrowLeftLong";
-import Image from "next/image";
+import useTranslation from 'next-translate/useTranslation';
+import { useCurrentUser, useLogout } from '@/api/UserApi';
+import { Button } from '@/components/Button/Button';
+import { NAV_LINKS } from '@/components/TopNavigation/navLinks';
+import { Text } from '@/components/Text/Text';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons/faRightFromBracket';
+import { SearchInput } from '@/components/SearchInput/SearchInput';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass';
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons/faArrowLeftLong';
+import Image from 'next/image';
 import logo from '@/assets/logo.png';
-import clsx from "clsx";
+import clsx from 'clsx';
+import styles from './TopNavigation.module.scss';
 
+export function TopNavigation() {
+  const { t } = useTranslation('navbar');
 
-export const TopNavigation = () => {
-    const {t} = useTranslation('navbar');
+  const { user } = useCurrentUser();
+  const logout = useLogout();
+  const [showMobileSearch, setShowMobileSearch] = useState(false);
 
-    const {user} = useCurrentUser();
-    const [showMobileSearch, setShowMobileSearch] = useState(false);
-
-    const onSearchButtonClick = () => setShowMobileSearch(prev => !prev);
-    const searchInput = <SearchInput
-        dropdownClassName={styles.searchDropdown}
-        fullWidth
-        multiSelect={false}
-        labelVisuallyHidden
-        placeholder={t('searchHint')}
+  const onSearchButtonClick = () => setShowMobileSearch((prev) => !prev);
+  const searchInput = (
+    <SearchInput
+      name="topSearch"
+      label={t('searchAriaLabel')}
+      labelVisuallyHidden
+      dropdownClassName={styles.searchDropdown}
+      fullWidth
+      multiSelect={false}
+      placeholder={t('searchHint')}
     />
+  );
 
-    return (
-        <nav className={styles.nav}>
-            <div className={clsx(styles.topBar, showMobileSearch && styles.topBarSearchActive)}>
-                {!showMobileSearch &&
+  return (
+    <nav className={styles.nav}>
+      <div className={clsx(styles.topBar, showMobileSearch && styles.topBarSearchActive)}>
+        {!showMobileSearch
+                    && (
                     <div className={styles.logoWithText}>
-                        <Image className={styles.logo} src={logo} alt='Logo'/>
-                        <Text fontSize={4}>{t('heading')}</Text>
+                      <Image className={styles.logo} src={logo} alt="Logo" />
+                      <Text fontSize={4}>{t('heading')}</Text>
                     </div>
-                }
-                <Button className={styles.mobileSearchToggle}
-                        icon={showMobileSearch ? faArrowLeftLong : faMagnifyingGlass} onClick={onSearchButtonClick}
-                        aria-label={t('searchAriaLabel')}/>
-                {showMobileSearch && searchInput}
-            </div>
-            {
-                !showMobileSearch &&
+                    )}
+        <Button
+          className={styles.mobileSearchToggle}
+          icon={showMobileSearch ? faArrowLeftLong : faMagnifyingGlass}
+          onClick={onSearchButtonClick}
+          aria-label={t('searchAriaLabel')}
+        />
+        {showMobileSearch && searchInput}
+      </div>
+      {
+                !showMobileSearch
+                && (
                 <ul className={styles.list}>
-                    {NAV_LINKS(user).map((link) => (
-                        <li key={link.href}>
-                            <Link
-                                className={styles.link}
-                                href={link.href}
-                            >
-                                <Text childClassName={styles.linkText}
-                                      fontSize={4}
-                                      icon={link.icon}>{t(link.key)}
-                                </Text>
-                            </Link>
-                        </li>
-                    ))}
-                    <li className={styles.desktopSearch}>
-                        {searchInput}
+                  {NAV_LINKS(user).map((link) => (
+                    <li key={link.href}>
+                      <Link
+                        className={styles.link}
+                        href={link.href}
+                      >
+                        <Text
+                          childClassName={styles.linkText}
+                          fontSize={4}
+                          icon={link.icon}
+                        >
+                          {t(link.key)}
+                        </Text>
+                      </Link>
                     </li>
-                    {user?.isLoggedIn &&
-                        <li className={styles.logOutBtn}><Button textClassName={styles.linkText}
-                                                                 icon={faRightFromBracket} fontSize={4} size='s'
-                                                                 variant="link_light">{t('logout')}</Button></li>
-                    }
+                  ))}
+                  <li className={styles.desktopSearch}>
+                    {searchInput}
+                  </li>
+                  {user?.isLoggedIn
+                        && (
+                        <li className={styles.logOutBtn}>
+                          <Button
+                            onClick={() => logout()}
+                            textClassName={styles.linkText}
+                            icon={faRightFromBracket}
+                            fontSize={4}
+                            size="s"
+                            variant="link_light"
+                          >
+                            {t('logout')}
+                          </Button>
+                        </li>
+                        )}
                 </ul>
+                )
             }
-        </nav>
-    )
+    </nav>
+  );
 }
--- components/TopNavigation/navLinks.ts
+++ components/TopNavigation/navLinks.ts
@@ -1,19 +1,19 @@
 import { faHouse } from '@fortawesome/free-solid-svg-icons/faHouse';
 import { faComments } from '@fortawesome/free-solid-svg-icons/faComments';
 import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
-import { faPenClip } from "@fortawesome/free-solid-svg-icons/faPenClip";
+import { faPenClip } from '@fortawesome/free-solid-svg-icons/faPenClip';
 import { faRightToBracket } from '@fortawesome/free-solid-svg-icons/faRightToBracket';
 
 const LOGGED_IN_LINKS = (user) => [
-    { key: 'profile', href: `/profile/${user?.id}`, icon: faUser },
-    { key: 'chat', href: '/chat', icon: faComments },
+  { key: 'profile', href: `/profile/${user?.id}`, icon: faUser },
+  { key: 'chat', href: '/chat', icon: faComments },
 ];
 
 const LOGGED_OUT_LINKS = [
-    { key: 'register', href: `${typeof window !== 'undefined' ? window.location.pathname : '/'}?showAuthModal=true`, icon: faRightToBracket },
-]
+  { key: 'register', href: '?showAuthModal=true', icon: faRightToBracket },
+];
 export const NAV_LINKS = (user) => [
-    { key: 'home', href: '/', icon: faHouse },
-    { key: 'post', href: '/stories/post', icon: faPenClip },
-    ...(user?.isLoggedIn ? LOGGED_IN_LINKS(user)  : LOGGED_OUT_LINKS),
-]
+  { key: 'home', href: '/', icon: faHouse },
+  { key: 'post', href: '/stories/post', icon: faPenClip },
+  ...(user?.isLoggedIn ? LOGGED_IN_LINKS(user) : LOGGED_OUT_LINKS),
+];
